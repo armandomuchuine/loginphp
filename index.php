6c307e5310623032fa89b86746624b85516881bc
armandomuchuine <87913148+armandomuchuine@users.noreply.github.com>
2023-03-07 08:30:41 +1400

    commit inicial
    esta e um login em php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="login.php" method="post">
+        <h2>Login </h2>
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+        <label >user name</label>
+        <input type="text" name="uname" placeholder="user name"><br>
+
+        <label >user name</label>
+        <input type="password" name="password" placeholder="password"><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
